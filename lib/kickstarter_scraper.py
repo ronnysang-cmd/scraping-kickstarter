4e094680b71cb18b1a3e68023e69a7136820112e
ronnysang <ronny.sang@student.moringaschool.com>
2026-01-13 12:00:52 +0300

    complete lab

--- lib/kickstarter_scraper.py
+++ lib/kickstarter_scraper.py
@@ -12,20 +12,26 @@ def create_project_dict():
     html = ''
     with open('./fixtures/kickstarter.html') as file:
         html = file.read()
+
     kickstarter = BeautifulSoup(html, 'html.parser')
     projects = {}
     # Iterate through the projects
     for project in kickstarter.select("li.project.grid_4"):
-        title = project.select("h2.bbcard_name strong a")[0].text
-        projects[title] = {
-        'image_link': project.select("div.project-thumbnail a img")[0]["src"],
-        'description': project.select("p.bbcard_blurb")[0].text,
-        'location': project.select("ul.project-meta span.location-name")[0].text,
-        'percent_funded': project.select("ul.project-stats li.first.funded strong")[0].text.replace("%","")
-        }
-    # return the projects dictionary
+        title_elem = project.select("h2.bbcard_name strong a")
+        img_elem = project.select("div.project-thumbnail a img")
+        desc_elem = project.select("p.bbcard_blurb")
+        loc_elem = project.select("ul.project-meta span.location-name")
+        fund_elem = project.select("ul.project-stats li.first.funded strong")
+        
+        if title_elem and img_elem and desc_elem and loc_elem and fund_elem:
+            title = title_elem[0].text
+            projects[title] = {
+                'image_link': img_elem[0]['src'],
+                'description': desc_elem[0].text,
+                'location': loc_elem[0].text,
+                'percent_funded': fund_elem[0].text.replace("%","")
+            }
 
+    # return the projects dictionary
     return projects
 
-projects = create_project_dict()
-print(projects)
